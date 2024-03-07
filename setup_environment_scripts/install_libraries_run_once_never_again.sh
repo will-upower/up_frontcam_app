855407962a4d42669b1109d06f5a088414e0d5cb
Jason Wu <jason.wu@upower.com>
2024-03-07 13:43:56 -0800

    cleaned everything up

--- setup_environment_scripts/install_libraries_run_once_never_again.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir -p /usr/lib/modules/5.10.147-yocto-standard/extra/
-sudo cp cmemdrv.ko /usr/lib/modules/5.10.147-yocto-standard/extra/cmemdrv.ko
-sudo mkdir -p /usr/lib/modules/5.10.147-yocto-standard/kernel/drivers/uio/
-sudo cp uio_pdrv_genirq.ko /usr/lib/modules/5.10.147-yocto-standard/kernel/drivers/uio/uio_pdrv_genirq.ko
--- setup_environment_scripts/load_kernel_modules_cmem_uio.sh
+++
@@ -1,4 +0,0 @@
-sudo rmmod cmemdrv
-sudo insmod /usr/lib/modules/5.10.147-yocto-standard/extra/cmemdrv.ko bsize=0x20000000
-sudo rmmod uio_pdrv_genirq
-sudo insmod /usr/lib/modules/5.10.147-yocto-standard/kernel/drivers/uio/uio_pdrv_genirq.ko of_id="generic-uio"
