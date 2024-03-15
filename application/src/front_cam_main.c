006fb25785064162557a6add1596d7675bba5aa8
Jason Wu <jason.wu@upower.com>
2024-03-15 12:21:36 -0700

    typo

--- application/src/front_cam_main.c
+++ application/src/front_cam_main.c
@@ -776,7 +776,7 @@ int64_t R_Capture_Task()
                     return FAILED;
                 }
 
-                //R_OSAL_ThreadSleepForTimePeriod ((osal_milli_sec_t)TIMEOUT_25MS_SLEEP);    
+                R_OSAL_ThreadSleepForTimePeriod ((osal_milli_sec_t)TIMEOUT_25MS_SLEEP);    
             }
             else if (true == g_customize.Image_Folder_Enable)       /* Image read from folder enabled */
             {
